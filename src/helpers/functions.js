8e7e5e587b144f563c579164295b541f2af2ec7f
EngeReyes23 <engelreyes23@gmail.com>
2022-01-01 20:46:20 -0600

    regexUrl modificado

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,5 +1,5 @@
 const regexUrl =
-	/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
+	/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_.~#?&=]*)/;
 
 // Valid that the URL is valid
 export const validateUrl = (url) => regexUrl.test(url);
